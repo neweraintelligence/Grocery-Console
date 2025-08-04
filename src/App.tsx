864487cd1dd90ca0ea5dbed8e5b2f3891baeed41
Simon Loewen <simon@neweraingelligence.com>
2025-08-04 11:49:42 -0600

    Increase header grocery scene icon by 25%
    - Change from 72px to 90px (72 * 1.25 = 90)
    
    - Maintains aspect ratio and object-fit contain
    
    - Better visual prominence for the main header icon

--- src/App.tsx
+++ src/App.tsx
@@ -2172,7 +2172,7 @@ function App() {
         <div style={styles.headerContent}>
           <div style={styles.logoSection}>
             <div style={styles.logoIcon}>
-              <img src="/grocery scene 1.png" alt="Grocery Scene" style={{width: '72px', height: '72px', objectFit: 'contain'}} />
+              <img src="/grocery scene 1.png" alt="Grocery Scene" style={{width: '90px', height: '90px', objectFit: 'contain'}} />
             </div>
             <div>
               <h1 style={styles.title}>Laurie's Legendary Kitchen</h1>
